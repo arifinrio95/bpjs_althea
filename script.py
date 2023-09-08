26ea0777bc6a39dc4731a69ecd1d4ef951bfcee2
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-08 15:55:05 +0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import openai
+
+openai.api_key = st.secrets['openai_apikey']
+
+class DokterAI:
+    def __init__(self):
+        self.messages = [
+            {"role": "system", "content": "Saya adalah DokterAI, dokter virtual Anda. Saya siap membantu Anda dengan pertanyaan medis Anda."}
+        ]
+
+    def get_reply(self, question):
+        self.messages.append({"role": "user", "content": question})
+        response = openai.ChatCompletion.create(
+            model='gpt-4',
+            messages=self.messages
+        )
+        answer = response['choices'][0]['message']['content']
+        self.messages.append({"role": "assistant", "content": answer})
+        return answer
+
+st.title("DokterAI")
+
+# Initialize DokterAI
+bot = DokterAI()
+
+# Create a placeholder for displaying conversation
+chat_history_placeholder = st.empty()
+
+# Create a text input for user input
+user_input = st.text_input("Tulis pertanyaan medis Anda di sini:")
+
+# Create a button for sending the question
+if st.button("Kirim"):
+    # Get a reply from the bot
+    response = bot.get_reply(user_input)
+
+    # Update the chat history
+    chat_str = ""
+    for message in bot.messages:
+        role, content = message["role"], message["content"]
+        if role == "user":
+            chat_str += f"User: {content}\n"
+        elif role == "assistant":
+            chat_str += f"DokterAI: {content}\n"
+    chat_history_placeholder.text(chat_str)
