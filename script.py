1fc0942e41d61b05a36dc17ece685c084d451bd5
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-08 16:13:10 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,66 +1,74 @@
 import streamlit as st
-import openai
+
+# Custom CSS to limit the width of the output
+st.markdown("""
+<style>
+    .output {
+        max-width: 600px;
+        word-wrap: break-word;
+    }
+</style>
+""", unsafe_allow_html=True)
 
 class DokterAI:
     def __init__(self, messages):
         self.messages = messages
 
     def get_reply(self, question):
-        self.messages.append({"role": "user", "content": question + """
-            Jika saya memberikan pernyataan selain symptomps, balas dengan normal.
-            Jika saya memberikan symptomps, berikan satu follow-up pertanyaan satu-persatu secara kreatif,
-            misal dari kebiasaan, riwayat, dan lainnya sampai kamu bisa mendiagnosa penyakit saya dengan pasti.
-            Jangan mendiagnosa jika kesimpulannya belum pasti. Setelah mendiagnosis,
-            sertakan sumber jurnal ilmiah atau paper yang mendukung beserta kutipannya yang related."""})
+        prompt = """Jika saya memberikan pernyataan selain symptomps, balas dengan normal.
+                    Jika saya memberikan symptomps: berikan satu follow-up pertanyaan satu-persatu secara kreatif misal dari kebiasaan, riwayat,
+                    dan lainnya sampai kamu bisa mendiagnosa penyakit saya dengan pasti,
+                    jangan mendiagnosa jika kesimpulannya belum pasti.
+                    Setelah mendiagnosis, sertakan sumber jurnal ilmiah atau paper yang mendukung beserta kutipannya yang related."""
+
+        self.messages.append({"role": "user", "content": question})
+        self.messages.append({"role": "system", "content": prompt})
 
         response = openai.ChatCompletion.create(
             model='gpt-4',
             messages=self.messages
         )
-
         answer = response['choices'][0]['message']['content']
         self.messages.append({"role": "assistant", "content": answer})
 
         return answer
 
-st.title("DokterAI")
-
-# Initialize session state if not already initialized
+# Initialize Streamlit session state
 if 'messages' not in st.session_state:
     st.session_state.messages = [
         {"role": "system", "content": "Saya adalah DokterAI, dokter virtual Anda. Saya siap membantu Anda dengan pertanyaan medis Anda."}
     ]
 
-# Initialize DokterAI with existing session messages
+# Initialize DokterAI
 bot = DokterAI(st.session_state.messages)
 
-# Create a placeholder for displaying conversation
+# Placeholder for chat
 chat_history_placeholder = st.empty()
 
 # Update the chat history
-chat_str = ""
+chat_str = "<div class='output'>"
 for message in bot.messages:
     role, content = message["role"], message["content"]
     if role == "user":
-        chat_str += f"User: {content}\n"
+        # Only display the user's input, not the entire prompt
+        chat_str += f"<p><strong>User:</strong> {content.split('. ')[0]}</p>"
     elif role == "assistant":
-        chat_str += f"DokterAI: {content}\n"
-chat_history_placeholder.text(chat_str)
+        chat_str += f"<p><strong>DokterAI:</strong> {content}</p>"
+chat_str += "</div>"
+chat_history_placeholder.markdown(chat_str, unsafe_allow_html=True)
 
-# Create a text input for user input
+# Text input and button
 user_input = st.text_input("Tulis pertanyaan medis Anda di sini:")
-
-# Create a button for sending the question
 if st.button("Kirim"):
-    # Get a reply from the bot
     response = bot.get_reply(user_input)
 
     # Update the chat history
-    chat_str = ""
+    chat_str = "<div class='output'>"
     for message in bot.messages:
         role, content = message["role"], message["content"]
         if role == "user":
-            chat_str += f"User: {content}\n"
+            chat_str += f"<p><strong>User:</strong> {content.split('. ')[0]}</p>"
         elif role == "assistant":
-            chat_str += f"DokterAI: {content}\n"
-    chat_history_placeholder.text(chat_str)
+            chat_str += f"<p><strong>DokterAI:</strong> {content}</p>"
+    chat_str += "</div>"
+    chat_history_placeholder.markdown(chat_str, unsafe_allow_html=True)
