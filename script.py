fc9982eb6a0bf6e155b4cc6b052d9026f59931ed
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-08 16:06:35 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,17 @@
 import streamlit as st
 import openai
 
-openai.api_key = st.secrets['openai_apikey']
-
 class DokterAI:
-    def __init__(self):
-        self.messages = [
-            {"role": "system", "content": "Saya adalah DokterAI, dokter virtual Anda. Saya siap membantu Anda dengan pertanyaan medis Anda."}
-        ]
+    def __init__(self, messages):
+        self.messages = messages
 
     def get_reply(self, question):
-        self.messages.append({"role": "user", "content": question+""". Jika saya memberikan pernyataan selain symptomps, balas dengan normal.
-                                                         Jika saya memberikan symptomps:berikan satu follow up pertanyaan satu-persatu secara kreatif misal dari kebiasaan, riwayat,
-                                                         dan lainnya sampai kamu bisa mendiagnosa penyakit saya dengan pasti,
-                                                         jangan mendiagnosa jika kesimpulannya belum pasti.
-                                                         Setelah mendiagnosis sertakan sumber jurnal ilmiah atau paper yang mendukung beserta kutipannya yang related."""})
+        self.messages.append({"role": "user", "content": question + """
+            Jika saya memberikan pernyataan selain symptomps, balas dengan normal.
+            Jika saya memberikan symptomps, berikan satu follow-up pertanyaan satu-persatu secara kreatif,
+            misal dari kebiasaan, riwayat, dan lainnya sampai kamu bisa mendiagnosa penyakit saya dengan pasti.
+            Jangan mendiagnosa jika kesimpulannya belum pasti. Setelah mendiagnosis,
+            sertakan sumber jurnal ilmiah atau paper yang mendukung beserta kutipannya yang related."""})
 
         response = openai.ChatCompletion.create(
             model='gpt-4',
@@ -26,14 +23,29 @@ class DokterAI:
 
         return answer
 
-
 st.title("DokterAI")
 
-# Initialize DokterAI
-bot = DokterAI()
+# Initialize session state if not already initialized
+if 'messages' not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": "Saya adalah DokterAI, dokter virtual Anda. Saya siap membantu Anda dengan pertanyaan medis Anda."}
+    ]
+
+# Initialize DokterAI with existing session messages
+bot = DokterAI(st.session_state.messages)
 
 # Create a placeholder for displaying conversation
 chat_history_placeholder = st.empty()
+
+# Update the chat history
+chat_str = ""
+for message in bot.messages:
+    role, content = message["role"], message["content"]
+    if role == "user":
+        chat_str += f"User: {content}\n"
+    elif role == "assistant":
+        chat_str += f"DokterAI: {content}\n"
+chat_history_placeholder.text(chat_str)
 
 # Create a text input for user input
 user_input = st.text_input("Tulis pertanyaan medis Anda di sini:")
